086de600e351ab7c0c84ba90501bc1acac5da51f
San4hi <antonsayfermay@gmail.com>
2023-11-05 12:37:10 +0300

    add sit and running realization

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -2,16 +2,24 @@ using Godot;
 using System;
 
 public partial class Player : CharacterBody3D
-{
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+{		
+	private enum States
+	{
+		NORMAL,
+		SIT,
+		RUNNING,
+		DEAD
+	}
 
-	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	private const float NormalSpeed = 5.0f;
+	private Vector3 NormalDetectionScale;
+	private const float JumpVelocity = 4.5f;
+	
+	private float Speed = NormalSpeed;
+	private float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	private States State = States.NORMAL;
 
-    public override void _Ready() =>
-		Position = GetNode<Node3D>("../SpawnPoint").Position;
-
-    public override void _PhysicsProcess(double delta)
+	private void Movement(ref double delta)
 	{
 		Vector3 velocity = Velocity;
 
@@ -21,8 +29,41 @@ public partial class Player : CharacterBody3D
 		if (Input.IsActionJustPressed("Jump") && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
+		if (Input.IsActionJustPressed("Sit") && IsOnFloor())
+		{
+			if (State == States.NORMAL || State == States.RUNNING) 
+			{
+				State = States.SIT;
+				Speed = NormalSpeed/2;
+				GetNode<CollisionShape3D>("DetectionCollisionShape").Scale = NormalDetectionScale/3;
+			}
+			else 
+			{
+				State = States.NORMAL;
+				Speed = NormalSpeed;
+				GetNode<CollisionShape3D>("DetectionCollisionShape").Scale = NormalDetectionScale;
+			}
+		}
+
+		if (Input.IsActionJustPressed("Running") && IsOnFloor())
+		{
+			if (State == States.NORMAL || State == States.SIT)
+			{
+				State = States.RUNNING;
+				Speed = NormalSpeed*2;
+				GetNode<CollisionShape3D>("DetectionCollisionShape").Scale = NormalDetectionScale*2;
+			}
+			else
+			{
+				State = States.NORMAL;
+				Speed = NormalSpeed;
+				GetNode<CollisionShape3D>("DetectionCollisionShape").Scale = NormalDetectionScale;
+			}
+		}
+
 		Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+
 		if (direction != Vector3.Zero)
 		{
 			velocity.X = direction.X * Speed;
@@ -33,8 +74,20 @@ public partial class Player : CharacterBody3D
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
 		}
-
+		GetNode<Hud>("../HUD").AviableLabels["Player speed"].Text = $"Player speed: {Speed.ToString()}";
+		GetNode<Hud>("../HUD").AviableLabels["Player velocity"].Text = $"Player velocity: {Velocity.ToString()}";
 		Velocity = velocity;
+	}
+
+    public override void _Ready()
+	{
+		Position = GetNode<Node3D>("../SpawnPoint").Position;
+		NormalDetectionScale = GetNode<Node3D>("DetectionCollisionShape").Scale;
+	}
+
+    public override void _PhysicsProcess(double delta)
+	{
+		Movement(ref delta);
 		MoveAndSlide();
 	}
 }
