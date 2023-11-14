b4dff5fbef0947abb84fb3ba378bae955ace58f0
San4hi <antonsayfermay@gmail.com>
2023-11-15 00:21:51 +0300

    optimize code

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -16,24 +16,30 @@ public partial class Player : CharacterBody3D
 	private float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
 	private Vector3 NormalDetectionScale;
-	private float Speed = NORMAL_SPEED;
+	private Vector3 DetectionScale;
 
-	public States State { get; set; }
+	private float Speed { get; set; } = NORMAL_SPEED;
+	public States State { get; set; } = States.NORMAL;
 
 	public override void _Ready()
 	{
-		State = States.NORMAL;
 		Position = GetNode<Node3D>("../SpawnPoint").Position;
-		NormalDetectionScale = GetNode<Node3D>("DetectionCollisionShape").Scale;
+
+		NormalDetectionScale = DetectionScale = GetNode<Node3D>("DetectionCollisionShape").Scale;	
 	}
 
 	public override void _PhysicsProcess(double delta)
-	{
-		Movement(ref delta);
+	{	
+		Vector2 MousePosition = GetViewport().GetMousePosition();
+		Vector2 LookPosition = MousePosition - GetViewport().GetVisibleRect().Size / 2; //Get Center of Screen
+
+		LookAt(new Vector3(LookPosition.X, Position.Y, LookPosition.Y));
+
+		Movement(delta);
 		MoveAndSlide();
 	}
 
-	private void Movement(ref double delta)
+	private void Movement(double delta)
 	{
 		Vector3 velocity = Velocity;
 
@@ -41,39 +47,13 @@ public partial class Player : CharacterBody3D
 			velocity.Y -= gravity * (float)delta;
 
 		if (Input.IsActionJustPressed("Jump") && IsOnFloor())
-			velocity.Y = JUMP_VELOCITY;
+			Jump(ref velocity);
 
 		if (Input.IsActionJustPressed("Sit") && IsOnFloor())
-		{
-			if (State == States.NORMAL || State == States.RUNNING) 
-			{
-				State = States.SITTING;
-				Speed = NORMAL_SPEED/2;
-				GetNode<CollisionShape3D>("DetectionCollisionShape").Scale = NormalDetectionScale/3;
-			}
-			else 
-			{
-				State = States.NORMAL;
-				Speed = NORMAL_SPEED;
-				GetNode<CollisionShape3D>("DetectionCollisionShape").Scale = NormalDetectionScale;
-			}
-		}
-
+			Sitting();
+		
 		if (Input.IsActionJustPressed("Running") && IsOnFloor())
-		{
-			if (State == States.NORMAL || State == States.SITTING)
-			{
-				State = States.RUNNING;
-				Speed = NORMAL_SPEED*2;
-				GetNode<CollisionShape3D>("DetectionCollisionShape").Scale = NormalDetectionScale*2;
-			}
-			else
-			{
-				State = States.NORMAL;
-				Speed = NORMAL_SPEED;
-				GetNode<CollisionShape3D>("DetectionCollisionShape").Scale = NormalDetectionScale;
-			}
-		}
+			Running();
 
 		Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
@@ -85,15 +65,53 @@ public partial class Player : CharacterBody3D
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, (float)delta * Speed * 2.5f);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, (float)delta * Speed * 2.5f);
 		}
-		GetNode<Hud>("../HUD").AviableLabels["Player speed"].Text = $"Player speed: {Speed.ToString()}";
-		GetNode<Hud>("../HUD").AviableLabels["Player velocity"].Text = $"Player velocity: {Velocity.ToString()}";
+
+		GetNode<Hud>("../HUD").AviableLabels["Player speed"].Text = $"Player speed: {Speed}";
+		GetNode<Hud>("../HUD").AviableLabels["Player velocity"].Text = $"Player velocity: {Velocity}";
 		Velocity = velocity;
 	}
 
+	private void Sitting() 
+	{
 
+		if (State != States.SITTING)
+		{
+			Speed = NORMAL_SPEED / 2;
+			State = States.SITTING;
+			DetectionScale = NormalDetectionScale / 3;
 
+		}
+		else
+		{
+			Speed = NORMAL_SPEED;
+            State = States.NORMAL;
+			DetectionScale = NormalDetectionScale;
+		}
+	}
 
+	private void Running()
+	{
+		if (State != States.RUNNING)
+        {
+            Speed = NORMAL_SPEED * 2;
+			State = States.RUNNING;
+            DetectionScale = NormalDetectionScale * 2;
+        }
+        else
+        {
+            State = States.NORMAL;
+            Speed = NORMAL_SPEED;
+            DetectionScale = NormalDetectionScale;
+        }
+	}
+
+	private void Jump(ref Vector3 velocity)
+	{
+		velocity.Y = JUMP_VELOCITY;
+		//Player animation = jumping
+		//Play
+	}
 }
